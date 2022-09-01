d7a1cf0704fa15add15bbb9c74de5e8124ba118f
aminosReact <a.mohammed@kyo-conseil.com>
2022-09-01 14:23:03 +0100

    add react

--- main.js
+++ main.js
@@ -8,12 +8,13 @@ const url=require('url');
 let win;
 function createWindow(){
     win=new BrowserWindow();
-    win.loadURL(url.format({
-        pathname:path.join(__dirname,'index.html'),
-        protocol:'file',
-        slashes:true
-    }));
+    // win.loadURL(url.format({
+    //     pathname:path.join(__dirname,'index.html'),
+    //     protocol:'file',
+    //     slashes:true
+    // }));
 
+    win.loadURL('https://vapeverstore.com/')
     win.webContents.openDevTools();
     
     win.on('closed',()=>{
